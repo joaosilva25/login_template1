55e5e569d26dfcc09d21cbb1a3a50ef6ffab735b
analu <joaosilva2505@outlook.comgit config --global user.name joaosilva25>
2023-04-29 10:21:58 -0300

    (second commit)

--- code.js
+++ code.js
@@ -1,7 +1,14 @@
-function cor {
-    const email=document.querySelector('.email');
+function view() {
     const senha=document.querySelector('.senha');
+    const img=document.querySelector('.imagem');
 
-
+    if (senha.getAttribute('type')==='text') {
+        senha.setAttribute('type','password');
+        img.setAttribute('src','assets/images/olho.png')
+    }
+    else {
+        senha.setAttribute ('type','text');
+        img.setAttribute('src','assets/images/olho_nulo.png')
+    }
 
 }
